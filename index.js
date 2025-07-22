699071120e685bc640009f2b085b6e2e7835cef9
chira-platforms <lukaspaydo@chira-platforms.com>
2025-07-21 20:27:10 -0700

    Update index.js
    Will be used in backend dev

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-export default {
-  async fetch(request) {
-    return new Response(`<!DOCTYPE html>
-      <html>
-        <head>
-          <title>Chira WebApp</title>
-        </head>
-        <body>
-          <h1>Welcome to Chira Platforms</h1>
-          <p>Your dynamic website is now live on Cloudflare Workers!</p>
-        </body>
-      </html>`, {
-      headers: { "Content-Type": "text/html" }
-    });
-  },
-};
+
