aadba11b1262cef145abbb95e579868f111aa434
chira-platforms <lukaspaydo@chira-platforms.com>
2026-05-13 15:16:32 -0700

    Update locality.js

--- public/locality/js/locality.js
+++ public/locality/js/locality.js
@@ -13,10 +13,9 @@ if (mapElement) {
     position: "bottomright"
   }).addTo(map);
 
-   L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
-      subdomains: "abcd",
-      maxZoom: 19
+   L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+     attribution: "&copy; OpenStreetMap contributors",
+     maxZoom: 19
    }).addTo(map);
 
    
